b560145cb9087fe7b0958c9f1b611e4b1628a64d
limtae <dkslsdkdl@naver.com>
2020-12-17 15:47:05 +0900

    Override bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Home from "./screen/Home";
-import "../src/assets/scss/styles.scss"
+
+import './assets/scss/styles.scss';
+
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
     <BrowserRouter>
